69d48bbf854c56ff5c711f9c748b96731ff8ccef
AslDeveloper07 <aslwexking777@gmail.com>
2025-06-19 06:55:59 +0500

    as

--- todo-list/script.js
+++ todo-list/script.js
@@ -32,14 +32,14 @@ function addTodo(todo) {
         todoEl.addEventListener('click', () => {
             todoEl.classList.toggle('completed')
             updateLS()
-        }) 
+        })
 
         todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault()
 
             todoEl.remove()
             updateLS()
-        }) 
+        })
 
         todosUL.appendChild(todoEl)
 
